fe6b225682d46971594887f9d9551baa994ba30e
Rootjang92 <janggeunho7@gmail.com>
2018-11-05 19:38:24 +0900

    react todo-list ver 1

---
+++ todo-list/src/components/TodoInput/TodoInput.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import styles from './TodoInput.scss';
+import classNames from 'classnames/bind';
+
+const cx = classNames.bind(styles);
+
+//
+/* value: input value
+   onChange : input change event
+   onInsert: add button event
+*/
+
+const TodoInput = ({value, onChange, onInsert}) => {
+  const handleKeyPress = (e) => {
+    if(e.key === 'Enter') {
+      onInsert();
+    }
+  }
+
+  return (
+    <div className={cx('todo-input')}>
+      <input onChange={onChange} value={value} onKeyPress={handleKeyPress}/>
+      <div className={cx('add-button')} onClick={onInsert}>추가</div>
+    </div>
+  );
+};
+
+export default TodoInput;
